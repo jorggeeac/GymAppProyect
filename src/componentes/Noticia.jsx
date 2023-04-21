6195df26ef4d2d706317111b5387636888dc4dbb
Jorge Igareda <39141568+JorgeIgareda@users.noreply.github.com>
2023-04-21 17:24:18 +0200

    actualizado
    añadida funcionalidad "leer mas" y componente de la noticia completa

--- src/componentes/Noticia.jsx
+++ src/componentes/Noticia.jsx
@@ -5,15 +5,16 @@ import '../hojas-de-estilo/Noticia.css';
 function Noticia({ id, titulo, fecha, imagen, resumen }) {
   return (
     <div className='contenedor-noticia'>
-      <div className='titulo-noticia'>{titulo}</div>
-      <div className='fecha-noticia'>{fecha}</div>
-      <img className='imagen-noticia' src={imagen} alt={titulo}/>
+      <a href={`noticias/${id}`}>
+        <div className='titulo-noticia'>{titulo}</div>
+        <div className='fecha-noticia'>{fecha}</div>
+      </a>
+      <img className='imagen-noticia' src={imagen} alt={titulo} />
       <div className='resumen-noticia'>{resumen}</div>
       <div className='contenedor-boton'>
-        <button className='leer-mas'>Leer más</button>
+        <a href={`noticias/${id}`}><button className='leer-mas'>Leer más</button></a>
       </div>
     </div>
   );
 }
-// TODO: Implementar funcionalidad del botón "Leer más"
 export default Noticia;
