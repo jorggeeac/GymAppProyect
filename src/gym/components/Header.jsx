47501cdb570862f7faba5c37042ffacf41488886
Jorge Igareda <39141568+JorgeIgareda@users.noreply.github.com>
2023-05-23 18:33:49 +0200

    Update Header.jsx

--- src/gym/components/Header.jsx
+++ src/gym/components/Header.jsx
@@ -4,7 +4,7 @@ import '../hojas-de-estilo/Header.css';
 const Header = () => {
   return (
     <header>
-      <h1>GymAppProyect</h1>
+      <h1>PocketGym</h1>
       <p>(Supongo que pondremos algún logo, o si no un div vacío para que no esté todo pegado arriba)</p>
     </header>
   );
